04c7993107ae824df6134d127bb95da87695b20f
unknown <raquelcarvalhogc@gmail.com>
2023-04-06 14:14:22 -0300

    exercicio 03

--- lista.py
+++ lista.py
@@ -5,4 +5,11 @@ print(exerc01)
 exerc02 = [10,9,8,7,6,5,4,3,2,1]
 print(exerc02)
 
-exerc03 = []
+
+exerc03 = [8,5,9,10,]
+media = 0
+for i in range(0,len(exerc03)):
+    print("nota", i=1, ":",exerc03[])
+    media = media + exerc03[i]
+media = media / len (exerc03)
+print("media:", media)

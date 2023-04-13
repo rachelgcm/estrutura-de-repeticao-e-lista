089f155a0fbbfdf032a146428ef9019b900d6ac4
unknown <raquelcarvalhogc@gmail.com>
2023-04-13 10:49:51 -0300

    exerc06

--- exerc03.py
+++ exerc03.py
@@ -1,15 +1,7 @@
-
-exerc01 = [1,2,3,4,5]
-print(exerc01) 
-
-exerc02 = [10,9,8,7,6,5,4,3,2,1]
-print(exerc02)
-
-
 exerc03 = [8,5,9,10,]
 media = 0
 for i in range(0,len(exerc03)):
-    print("nota", i=1, ":",exerc03[])
+    print("nota", i+1, ":",exerc03[i])
     media = media + exerc03[i]
 media = media / len (exerc03)
 print("media:", media)
---
+++ exerc04.py
@@ -0,0 +1,4 @@
+exerc04 = ["a","b","c","d","e","f","g","h","i","j"]
+for i in exerc04:
+    if i not in "a,e,i,o,u":
+        print(i)

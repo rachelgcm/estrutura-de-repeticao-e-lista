9b45cf36efa0442bc9eee41aedad5191968443b8
unknown <raquelcarvalhogc@gmail.com>
2023-04-14 14:38:00 -0300

    exerc04Rep

--- exerc2Rep.py
+++ exerc2Rep.py
@@ -1,10 +1,10 @@
 
 print("faça seu cadastro:")
-usuario=str(input("usuario--> "))
-senha=str(input("senha-->"))
+usuario=str(input("usuario"))
+senha=str(input("senha"))
 while usuario==senha:
 	print("ERRO: o usuario nao pode ser igual a senha.")
-	usuario=str(input("usuario--> "))
-	senha=str(input("senha-->"))
+	usuario=str(input("usuariO"))
+	senha=str(input("senha"))
 else:
 	print("cadastrado realizado com sucesso")

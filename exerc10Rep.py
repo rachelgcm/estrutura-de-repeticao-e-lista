7c74cb1a97c935d9493224288a2f705ad515177c
unknown <raquelcarvalhogc@gmail.com>
2023-04-14 16:53:57 -0300

    exerc10Rep

---
+++ exerc10Rep.py
@@ -0,0 +1,10 @@
+
+numero1=int(input("escreva um numero"))
+numero2=int(input("escreva outro numero"))
+
+while num2<num1:
+	numero1=int(input("escreva um numero"))
+	numero2=int(input("escreva outro numero"))
+else:
+	for i in range(numero1,numero2):
+		print(i)
